e15f7dd358944e4e7b96f4d2cc8e1159682d15f6
adarshdabral <106724982+adarshdabral@users.noreply.github.com>
2024-11-22 14:53:36 +0530

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 
+
+# | Data Science Tools       |
+# |--------------------------|
+# | Jupyter Notebook         |
+# | RStudio                  |
+# | Apache Zeppelin          |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+
+# In[3]:
+
+
+# This is a simple arithmetic expression to multiply and add integers
+(3 * 4) + 5
+
+
+# In[4]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+200 / 60
+
+
+# **Objectives:**
+# - List popular languages for Data Science.
+# - Identify commonly used libraries in Data Science.
+# - Highlight key Data Science tools.
+# - Provide examples of arithmetic operations in Python.
+# 
+
+# ## Author
+# Adarsh Dabral
+# 
+
+# # Data Science Tools and Ecosystem
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Scikit-learn
+# 
+
+# | Data Science Tools       |
+# |--------------------------|
+# | Jupyter Notebook         |
+# | RStudio                  |
+# | Apache Zeppelin          |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+
+# In[7]:
+
+
+# This is a simple arithmetic expression to multiply and add integers
+(3 * 4) + 5
+
+
+# In[8]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+200 / 60
+
+
+# **Objectives:**
+# - List popular languages for Data Science.
+# - Identify commonly used libraries in Data Science.
+# - Highlight key Data Science tools.
+# - Provide examples of arithmetic operations in Python.
+# 
+
+# ## Author
+# Adarsh
